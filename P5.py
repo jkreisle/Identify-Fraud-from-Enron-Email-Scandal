dcf1eeed7331757e0d98cbb41f97739fa7098b5a
jkreisle <Jacob@Jacobs-MacBook-Air.local>
2017-01-18 14:51:34 -0500

    Identify Fraud from Enron Email Scandal
    The goal of this project is to identify patterns in a dataset of Enron
    employees so that we can predict persons of interest, or POIs, in the
    fraud case. With machine learning, we can build powerful algorithms
    that take in data features and return predictions.

---
+++ P5.py
@@ -0,0 +1,252 @@
+
+# coding: utf-8
+
+# In[71]:
+
+#!/usr/bin/python
+
+import sys
+import pickle
+import os
+import pprint
+import numpy
+from sklearn.metrics import classification_report
+import pylab 
+import matplotlib.pyplot
+
+
+os.chdir('/Users/Jacob/Downloads/tools')
+from feature_format import featureFormat, targetFeatureSplit
+
+os.chdir('/Users/Jacob/Downloads/ud120-projects-master/final_project')
+from tester import dump_classifier_and_data
+
+### Task 1: Select what features you'll use.
+### features_list is a list of strings, each of which is a feature name.
+### The first feature must be "poi".
+
+
+features_list = ['poi', 'salary', 'total_payments', 'exercised_stock_options', 
+                 'to_ratio', 'bonus', 'total_stock_value', 'expenses', 
+                 'loan_advances', 'deferred_income', 'long_term_incentive'] 
+            
+
+
+### Load the dictionary containing the dataset
+
+
+with open("final_project_dataset.pkl", "r") as data_file:
+    data_dict = pickle.load(data_file)
+    
+
+### Task 2: Remove outliers
+
+# Use to identify outlier
+"""
+features = ["salary", "bonus"]
+data = featureFormat(data_dict, features)
+for point in data:
+    salary = point[0]
+    bonus = point[1]
+    matplotlib.pyplot.scatter( salary, bonus )
+matplotlib.pyplot.xlabel("salary")
+matplotlib.pyplot.ylabel("bonus")
+matplotlib.pyplot.show()
+"""
+
+# Remove outlier
+data_dict.pop('TOTAL', 0)
+
+
+### Task 3: Create new feature(s)
+### Store to my_dataset for easy export below.
+
+my_dataset = data_dict
+
+
+for key, value in my_dataset.iteritems():
+    value['to_ratio'] = 0
+    if value['to_messages'] and value['from_this_person_to_poi'] != 'NaN':
+        value['from_this_person_to_poi'] = 
+        float(value['from_this_person_to_poi'])
+        value['to_messages'] = float(value['to_messages'])
+        if value['from_this_person_to_poi'] > 0:
+            value['to_ratio'] = value['from_this_person_to_poi'] / 
+            value['to_messages']
+            
+             
+
+### Extract features and labels from dataset for local testing
+data = featureFormat(my_dataset, features_list, sort_keys = True)
+labels, features = targetFeatureSplit(data)
+
+
+### Task 4: Try a varity of classifiers
+### Please name your classifier clf for easy export below.
+### Note that if you want to do PCA or other multi-stage operations,
+### you'll need to use Pipelines. For more info:
+### http://scikit-learn.org/stable/modules/pipeline.html
+
+# Provided to give you a starting point. Try a variety of classifiers.
+
+
+
+from sklearn.ensemble import AdaBoostClassifier
+clf = AdaBoostClassifier(n_estimators=250)
+
+    
+### Task 5: Tune your classifier to achieve better than .3 precision and recall 
+### using our testing script. Check the tester.py script in the final project
+### folder for details on the evaluation method, especially the test_classifier
+### function. Because of the small size of the dataset, the script uses
+### stratified shuffle split cross validation. For more info: 
+### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
+
+
+# Example starting point. Try investigating other evaluation techniques!
+from sklearn.cross_validation import train_test_split
+features_train, features_test, labels_train, labels_test =     
+train_test_split(features, labels, test_size=0.25, random_state=42)
+
+
+
+# Parameter tuning    
+"""
+from sklearn.grid_search import GridSearchCV
+parameters = {'n_estimators':[50,100,150,200,250,300,350,400]}
+ada = AdaBoostClassifier()
+clf = GridSearchCV(ada, parameters)
+clf.fit(features_train, labels_train)
+print(clf.best_params_)
+"""
+
+
+
+# Feature importance    
+"""
+from sklearn import metrics
+from sklearn.ensemble import ExtraTreesClassifier
+clf = ExtraTreesClassifier()
+clf.fit(features_train, labels_train)
+print(clf.feature_importances_)
+"""
+
+
+### Task 6: Dump your classifier, dataset, and features_list so anyone can
+### check your results. You do not need to change anything below, but make sure
+### that the version of poi_id.py that you submit can be run on its own and
+### generates the necessary .pkl files for validating your results.
+
+dump_classifier_and_data(clf, my_dataset, features_list)
+
+
+# In[72]:
+
+#!/usr/bin/pickle
+
+""" a basic script for importing student's POI identifier,
+    and checking the results that they get from it 
+ 
+    requires that the algorithm, dataset, and features list
+    be written to my_classifier.pkl, my_dataset.pkl, and
+    my_feature_list.pkl, respectively
+
+    that process should happen at the end of poi_id.py
+"""
+
+import pickle
+import sys
+from sklearn.cross_validation import StratifiedShuffleSplit
+sys.path.append("../tools/")
+from feature_format import featureFormat, targetFeatureSplit
+
+PERF_FORMAT_STRING = "\tAccuracy: {:>0.{display_precision}f}\tPrecision: {:>0.{display_precision}f}\tRecall: {:>0.{display_precision}f}\tF1: {:>0.{display_precision}f}\tF2: {:>0.{display_precision}f}"
+RESULTS_FORMAT_STRING = "\tTotal predictions: {:4d}\tTrue positives: {:4d}\tFalse positives: {:4d}\tFalse negatives: {:4d}\tTrue negatives: {:4d}"
+
+def test_classifier(clf, dataset, feature_list, folds = 1000):
+    data = featureFormat(dataset, feature_list, sort_keys = True)
+    labels, features = targetFeatureSplit(data)
+    cv = StratifiedShuffleSplit(labels, folds, random_state = 42)
+    true_negatives = 0
+    false_negatives = 0
+    true_positives = 0
+    false_positives = 0
+    for train_idx, test_idx in cv: 
+        features_train = []
+        features_test  = []
+        labels_train   = []
+        labels_test    = []
+        for ii in train_idx:
+            features_train.append( features[ii] )
+            labels_train.append( labels[ii] )
+        for jj in test_idx:
+            features_test.append( features[jj] )
+            labels_test.append( labels[jj] )
+        
+        ### fit the classifier using training set, and test on test set
+        clf.fit(features_train, labels_train)
+        predictions = clf.predict(features_test)
+        for prediction, truth in zip(predictions, labels_test):
+            if prediction == 0 and truth == 0:
+                true_negatives += 1
+            elif prediction == 0 and truth == 1:
+                false_negatives += 1
+            elif prediction == 1 and truth == 0:
+                false_positives += 1
+            elif prediction == 1 and truth == 1:
+                true_positives += 1
+            else:
+                print "Warning: Found a predicted label not == 0 or 1."
+                print "All predictions should take value 0 or 1."
+                print "Evaluating performance for processed predictions:"
+                break
+    try:
+        total_predictions = true_negatives + false_negatives + false_positives + true_positives
+        accuracy = 1.0*(true_positives + true_negatives)/total_predictions
+        precision = 1.0*true_positives/(true_positives+false_positives)
+        recall = 1.0*true_positives/(true_positives+false_negatives)
+        f1 = 2.0 * true_positives/(2*true_positives + false_positives+false_negatives)
+        f2 = (1+2.0*2.0) * precision*recall/(4*precision + recall)
+        print clf
+        print PERF_FORMAT_STRING.format(accuracy, precision, recall, f1, f2, display_precision = 5)
+        print RESULTS_FORMAT_STRING.format(total_predictions, true_positives, false_positives, false_negatives, true_negatives)
+        print ""
+    except:
+        print "Got a divide by zero when trying out:", clf
+        print "Precision or recall may be undefined due to a lack of true positive predicitons."
+
+CLF_PICKLE_FILENAME = "my_classifier.pkl"
+DATASET_PICKLE_FILENAME = "my_dataset.pkl"
+FEATURE_LIST_FILENAME = "my_feature_list.pkl"
+
+def dump_classifier_and_data(clf, dataset, feature_list):
+    with open(CLF_PICKLE_FILENAME, "w") as clf_outfile:
+        pickle.dump(clf, clf_outfile)
+    with open(DATASET_PICKLE_FILENAME, "w") as dataset_outfile:
+        pickle.dump(dataset, dataset_outfile)
+    with open(FEATURE_LIST_FILENAME, "w") as featurelist_outfile:
+        pickle.dump(feature_list, featurelist_outfile)
+
+def load_classifier_and_data():
+    with open(CLF_PICKLE_FILENAME, "r") as clf_infile:
+        clf = pickle.load(clf_infile)
+    with open(DATASET_PICKLE_FILENAME, "r") as dataset_infile:
+        dataset = pickle.load(dataset_infile)
+    with open(FEATURE_LIST_FILENAME, "r") as featurelist_infile:
+        feature_list = pickle.load(featurelist_infile)
+    return clf, dataset, feature_list
+
+def main():
+    ### load up student's classifier, dataset, and feature_list
+    clf, dataset, feature_list = load_classifier_and_data()
+    ### Run testing script
+    test_classifier(clf, dataset, feature_list)
+
+if __name__ == '__main__':
+    main()
+
+
+# In[ ]:
+
+
+
